953c65a2a57299def50856951a4a744176bbb29d
Mateusz Sobiech <lordpython8@gmail.com>
2022-06-19 13:02:43 +0200

    Create collateral views.

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 import { setFormSubmitEvent } from './scripts/email.js';
+import { loadRecentVideos } from './scripts/iFrame.js';
 
 setFormSubmitEvent();
+loadRecentVideos();
+
+// https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCFGud6qsbaAjuP6WaXAJj3Q&maxResults=9&order=date&type=video&key=AIzaSyC0BySpEn4IEB3leni5800SA5MjYEfNNpY
